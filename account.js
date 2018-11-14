8b44e31bf92883bb976a5cc9ed9264f91bdc19fa
kpratico <praticok@oregonstate.edu>
2018-11-13 21:31:31 -0800

    adding accounts, login, register

---
+++ account.js
@@ -0,0 +1,37 @@
+
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+    var Handlebars = require('handlebars');
+
+    router.get('/', function(req, res){
+        res.render('accounts');
+    });
+
+
+    router.post('/', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "SELECT user_name FROM Accounts WHERE user_pw = ?";
+        var inserts = [req.body.password];        
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                console.log(results[0])
+                if (results[0] != undefined) {
+                    res.redirect('/');
+                }
+                else {
+                    res.redirect('/accounts');
+                }
+            }
+        });
+    });
+
+
+    return router;
+
+}();
---
+++ dbcon.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs361_praticok',
+  password        : '4777',
+  database        : 'cs361_praticok'
+});
+
+module.exports.pool = pool;
